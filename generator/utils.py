dc89e77914c7e7c850f5b5ac020c3e873e93719c
dmytroshurko <dmytro.shurko@protonmail.com>
2022-01-14 19:22:35 +0200

    update docstrings

--- generator/utils.py
+++ generator/utils.py
@@ -5,24 +5,24 @@ import json
 
 
 def save_data(data: list, filepath: str) -> None:
-    """Save the dataset to a specific location.
+    """Save data to a specific location.
 
     Args:
-        data (list): A list to save.
-        filepath (str): Location to save the list to as a JSON file.
+        data (list): A list (or dictionary) to save.
+        filepath (str): Location to save the list (or dictionary) to as a JSON file.
     """
     with open(filepath, "w") as fp:
         json.dump(data, indent=2, fp=fp)
 
 
 def load_data(filepath: str) -> list:
-    """Load a list from a JSON's filepath.
+    """Load a list (or dictionary) from a JSON's filepath.
 
     Args:
         filepath (str): JSON's filepath.
 
     Returns:
-        A list with the data loaded.
+        A list (or dictionary) with the data loaded.
     """
     with open(filepath) as fp:
         data = json.load(fp)
