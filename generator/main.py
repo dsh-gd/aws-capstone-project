acad020a0daaf9d842019fd6772b9084d7110ffc
dmytroshurko <dmytro.shurko@protonmail.com>
2022-01-16 21:08:16 +0200

    rename variables and functions names

--- generator/main.py
+++ generator/main.py
@@ -12,11 +12,11 @@ app = typer.Typer()
 
 
 @app.command()
-def users_ids(
-    data_fp: Path = Path(config.DATA_DIR, "users_ids.json"), size: int = 1000
+def user_ids(
+    data_fp: Path = Path(config.DATA_DIR, "user_ids.json"), size: int = 1000
 ) -> None:
-    users_ids = data.generate_users_ids(size)
-    utils.save_data(users_ids, data_fp)
+    user_ids = data.generate_user_ids(size)
+    utils.save_data(user_ids, data_fp)
 
 
 @app.command()
@@ -33,18 +33,18 @@ def items(
 @app.command()
 def user_actions(
     data_fp: Path = Path(config.DATA_DIR, "user_actions.json"),
-    users_ids_fp: Path = Path(config.DATA_DIR, "users_ids.json"),
+    user_ids_fp: Path = Path(config.DATA_DIR, "user_ids.json"),
     items_fp: Path = Path(config.DATA_DIR, "items.json"),
     size: int = 1000,
     params_fp: Path = Path(config.CONFIG_DIR, "params.json"),
 ):
     params = Namespace(**utils.load_data(filepath=params_fp))
 
-    users_ids = utils.load_data(users_ids_fp)
+    user_ids = utils.load_data(user_ids_fp)
     items = utils.load_data(items_fp)
     items_ids = [item["id"] for item in items]
 
     user_actions = data.generate_user_actions(
-        params, users_ids, items_ids, size
+        params, user_ids, items_ids, size
     )
     utils.save_data(user_actions, data_fp)
