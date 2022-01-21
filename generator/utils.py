72ab0fc6ef361b0b4fe8e83ee5b86f406b612e42
dmytroshurko <dmytro.shurko@protonmail.com>
2022-01-21 13:17:29 +0200

    update functions to work with S3

--- generator/utils.py
+++ generator/utils.py
@@ -3,91 +3,54 @@
 
 import datetime
 import json
+import random
+import re
 from argparse import Namespace
 from pathlib import Path
 
 import boto3
 
 
-def save_data(data: list, filepath: str) -> None:
-    """Save data to a specific location.
-
-    Args:
-        data (list): A list (or dictionary) to save.
-        filepath (str): Location to save the list (or dictionary) to as a JSON file.
-    """
-    with open(filepath, "w") as fp:
-        json.dump(data, indent=2, fp=fp)
-
-
-def load_data(filepath: str) -> list:
-    """Load a list (or dictionary) from a JSON's filepath.
-
+def load_data(filepath: str) -> dict:
+    """Load a dictionary from a JSON's filepath.
     Args:
         filepath (str): JSON's filepath.
-
     Returns:
-        A list (or dictionary) with the data loaded.
+        A dictionary with the data loaded.
     """
     with open(filepath) as fp:
         data = json.load(fp)
     return data
 
 
-def dt_path(prefix: str, ext: str = ".json") -> str:
-    """Create the path of the file using the date and time.
+def dt_path(prefix: str, dt: datetime.datetime = None) -> str:
+    """Create path for the file using date and time.
 
     Args:
         prefix (str): A prefix for the path.
-        ext (str): An extension of the file. (Default is ".json")
+        dt (datetime.datetime, optional): Date and time.
 
     Returns:
-        The path to the file.
-
-    Example:
-        item/2022/01/20/13.json
+        The path to the file without extension.
+        prefix/year/month/day/hour/timestamp
     """
-    now = datetime.datetime.now()
-    date = now.strftime("%Y/%m/%d")
-    fname = f"{now.hour}{ext}"
-    path = Path(prefix, date, fname)
+    if not dt:
+        dt = datetime.datetime.now()
+    dt_str = dt.strftime("%Y/%m/%d/%H")
+    attempt_id = dt.strftime("%Y%m%d%H%M%S")
+    path = Path(prefix, dt_str, attempt_id)
     return str(path)
 
 
-def save_data_s3(aws_config: Namespace, data: list, dprefix: str) -> None:
-    """Save data to a bucket on S3.
+def latest_dset_path(aws_config: Namespace, dset_type: str) -> str:
+    """Get path of the latest dataset of a given type on the S3.
 
     Args:
         aws_config (Namespace): AWS credentials and name of the bucket.
-        data (list): A list (or dictionary) to save.
-        dprefix (str): A prefix for the dataset path.
-    """
-    s3 = boto3.resource(
-        "s3",
-        region_name=aws_config.region_name,
-        aws_access_key_id=aws_config.aws_access_key_id,
-        aws_secret_access_key=aws_config.aws_secret_access_key,
-    )
-    path = dt_path(dprefix)
-    s3_obj = s3.Object(aws_config.bucket_name, path)
-    s3_obj.put(
-        Body=(bytes(json.dumps(data).encode("UTF-8"))),
-        ContentType="application/json",
-    )
-
-
-def collect_values_s3(
-    aws_config: Namespace, dprefix: str, key: str = None
-) -> list:
-    """Collect data values from the dataset.
-
-    Args:
-        aws_config (Namespace): AWS credentials and name of the bucket.
-        dprefix (str): A prefix for the dataset path.
-        key (str, optional): A key for collecting values from the dictionaries.
+        dset_type (str): A type of the dataset.
 
     Returns:
-        A list with the data values.
+        Base path of the latest dataset.
     """
     s3 = boto3.resource(
         "s3",
@@ -97,12 +60,72 @@ def collect_values_s3(
     )
     bucket = s3.Bucket(aws_config.bucket_name)
 
-    values = []
-    for obj in bucket.objects.filter(Prefix=dprefix):
-        data = json.load(obj.get()["Body"])
-        if key:
-            values.extend([o[key] for o in data])
-        else:
-            values.extend(data)
+    objects = bucket.objects.filter(Prefix=dset_type)
+    paths = sorted([obj.key for obj in objects if obj.key.endswith(".json")])
+    if paths:
+        p = re.match(r"(.+/\d+)(_available|_unavailable)?.json", paths[-1])
+        path = p.groups()[0]
+        return path
+    return None
 
-    return values
+
+def save_data_s3(aws_config: Namespace, data: list, path: str) -> None:
+    """Save data to a bucket on S3.
+
+    Args:
+        aws_config (Namespace): AWS credentials and name of the bucket.
+        data (list): A list (or dictionary) to save.
+        path (str): Path to the file.
+    """
+    s3 = boto3.resource(
+        "s3",
+        region_name=aws_config.region_name,
+        aws_access_key_id=aws_config.aws_access_key_id,
+        aws_secret_access_key=aws_config.aws_secret_access_key,
+    )
+    obj = s3.Object(aws_config.bucket_name, path)
+    obj.put(
+        Body=(bytes(json.dumps(data).encode("UTF-8"))),
+        ContentType="application/json",
+    )
+
+
+def load_data_s3(aws_config: Namespace, path: str) -> list:
+    """Load data from the bucket on S3.
+
+    Args:
+        aws_config (Namespace): AWS credentials and name of the bucket.
+        path (str): Path to the file.
+
+    Returns:
+        A list with the data loaded.
+    """
+    s3 = boto3.resource(
+        "s3",
+        region_name=aws_config.region_name,
+        aws_access_key_id=aws_config.aws_access_key_id,
+        aws_secret_access_key=aws_config.aws_secret_access_key,
+    )
+    obj = s3.Object(aws_config.bucket_name, path)
+    file_content = obj.get()["Body"].read().decode("utf-8")
+    data = json.loads(file_content)
+    return data
+
+
+def to_delete(elements: list, n_del: int) -> set:
+    """Return set with elements to delete.
+
+    Args:
+        elements (list): List with elements.
+        n_del (int): Number of elements to delete.
+
+    Returns:
+        Set with elements to delete.
+    """
+    n = len(elements)
+    if n_del > n:
+        del_idxs = []
+    else:
+        del_idxs = random.sample(range(len(elements)), n_del)
+    to_del = {elem for idx, elem in enumerate(elements) if idx in del_idxs}
+    return to_del
