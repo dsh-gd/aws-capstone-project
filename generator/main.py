7ca25ffb8ce835609285550659d47ef24d7ffb55
dmytroshurko <dmytro.shurko@protonmail.com>
2022-01-25 21:36:26 +0200

    delete aws_config

--- generator/main.py
+++ generator/main.py
@@ -13,32 +13,24 @@ app = typer.Typer()
 
 
 @app.command()
-def user_ids(
-    aws_config_fp: Path = Path(config.CONFIG_DIR, "aws_config.json"),
-    size: int = 1000,
-):
-    aws_config = Namespace(**utils.load_data(filepath=aws_config_fp))
-    update.user_ids_dset(aws_config, size)
+def user_ids(size: int = 1000):
+    update.user_ids_dset(size)
 
 
 @app.command()
 def items(
-    aws_config_fp: Path = Path(config.CONFIG_DIR, "aws_config.json"),
     params_fp: Path = Path(config.CONFIG_DIR, "generator_params.json"),
     size: int = 1000,
     n_del: int = 5,
 ):
-    aws_config = Namespace(**utils.load_data(filepath=aws_config_fp))
     params = Namespace(**utils.load_data(filepath=params_fp))
-    update.items_dset(aws_config, params, size, n_del)
+    update.items_dset(params, size, n_del)
 
 
 @app.command()
 def user_actions(
-    aws_config_fp: Path = Path(config.CONFIG_DIR, "aws_config.json"),
     params_fp: Path = Path(config.CONFIG_DIR, "generator_params.json"),
     size: int = 1000,
 ):
-    aws_config = Namespace(**utils.load_data(filepath=aws_config_fp))
     params = Namespace(**utils.load_data(filepath=params_fp))
-    update.user_actions_dset(aws_config, params, size)
+    update.user_actions_dset(params, size)
