d120a4f10752cafe7cd77560e45743607d0c5a91
dmytroshurko <dmytro.shurko@protonmail.com>
2022-01-25 21:37:47 +0200

    delete aws_config

--- generator/update.py
+++ generator/update.py
@@ -7,27 +7,22 @@ from argparse import Namespace
 from generator import data, utils
 
 
-def user_ids_dset(aws_config: Namespace, size: int = 1000) -> None:
+def user_ids_dset(size: int = 1000) -> None:
     user_ids = data.generate_user_ids(size)
     path = utils.dt_path("user_ids") + ".json"
-    utils.save_data_s3(aws_config, user_ids, path)
+    utils.save_data_s3(user_ids, path)
 
 
 def delete_items(
-    aws_config: Namespace,
     n_del: int = 5,
     new_available: list = [],
     dt: datetime.datetime = None,
 ) -> None:
     dset_prefix = "items"
-    base_lst_path = utils.latest_path(
-        aws_config, dset_prefix, "_available", dt
-    )
+    base_lst_path = utils.latest_path(dset_prefix, "_available", dt)
 
     if base_lst_path:
-        available = set(
-            utils.load_data_s3(aws_config, base_lst_path + "_available.json")
-        )
+        available = set(utils.load_data_s3(base_lst_path + "_available.json"))
     else:
         available = set()
 
@@ -36,15 +31,12 @@ def delete_items(
 
     base_path = utils.dt_path(dset_prefix, dt)
     utils.save_data_s3(
-        aws_config, list(leave) + new_available, base_path + "_available.json"
+        list(leave) + new_available, base_path + "_available.json"
     )
-    utils.save_data_s3(
-        aws_config, list(delete), base_path + "_unavailable.json"
-    )
+    utils.save_data_s3(list(delete), base_path + "_unavailable.json")
 
 
 def items_dset(
-    aws_config: Namespace,
     params: Namespace,
     size: int = 1000,
     n_del: int = 5,
@@ -53,26 +45,24 @@ def items_dset(
     items = data.generate_items(params, size)
 
     base_path = utils.dt_path("items", dt)
-    utils.save_data_s3(aws_config, items, base_path + ".json")
+    utils.save_data_s3(items, base_path + ".json")
 
     new_available = [item["id"] for item in items]
-    delete_items(aws_config, n_del, new_available, dt)
+    delete_items(n_del, new_available, dt)
 
 
-def user_actions_dset(
-    aws_config: Namespace, params: Namespace, size: int = 1000
-) -> None:
-    user_ids_path = utils.latest_path(aws_config, "user_ids") + ".json"
-    user_ids = utils.load_data_s3(aws_config, path=user_ids_path)
+def user_actions_dset(params: Namespace, size: int = 1000) -> None:
+    user_ids_path = utils.latest_path("user_ids") + ".json"
+    user_ids = utils.load_data_s3(path=user_ids_path)
 
     base_items_path = utils.latest_path(
-        aws_config, dset_prefix="items", dset_type="_available"
+        dset_prefix="items", dset_type="_available"
     )
     items_path = base_items_path + "_available.json"
-    items_ids = utils.load_data_s3(aws_config, path=items_path)
+    items_ids = utils.load_data_s3(path=items_path)
 
     user_actions = data.generate_user_actions(
         params, user_ids, items_ids, size
     )
     path = utils.dt_path("user_actions") + ".json"
-    utils.save_data_s3(aws_config, data=user_actions, path=path)
+    utils.save_data_s3(data=user_actions, path=path)
