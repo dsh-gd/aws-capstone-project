9385951cc4bb0f4c3cad54c8618d686dcc358b53
dmytroshurko <dmytro.shurko@protonmail.com>
2022-01-27 18:39:02 +0200

    download numpy as a precompiled package and update script

--- build_layer.sh
+++ build_layer.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
+# NumPy 1.21.5
+NUMPY="https://files.pythonhosted.org/packages/f8/cc/038b8277fe977ae1f18d11660386af6093547d6c0bd9a9fadbed4795091a/numpy-1.21.5-cp39-cp39-manylinux_2_12_x86_64.manylinux2010_x86_64.whl"
+
 rm -f package.zip
 
 mkdir -p package/python
 
-python3 -m venv venv
-source venv/bin/activate
-pip install --target package/python -r requirements.txt
-deactivate
-rm -rf package/python/{*dist-info,__pycache__}
-
 cp generator/{data,utils}.py package/python
 cp config/generator_params.json package
 
+python3 -m venv venv
+source venv/bin/activate
+pip install --target package/python Faker==10.0.0
+deactivate
+
+wget $NUMPY
+unzip numpy*.whl -d package/python
+
 zip -r package.zip package
 
+rm numpy*.whl
 rm -rf package
